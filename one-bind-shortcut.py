1375bd99c92e88040ed3d4683aa0b930bf43abae
Adam Magnier <github@magnier.io>
2023-06-24 13:33:22 +0200

    terminator 2.1.3

--- one-bind-shortcut.py
+++ one-bind-shortcut.py
@@ -1,18 +1,23 @@
 import terminatorlib.plugin as plugin
-import os
-import time
 from terminatorlib.translation import _
 from terminatorlib.terminator import Terminator
-from terminatorlib.version import APP_VERSION, APP_NAME
+import gi
+from gi.repository import Gtk, Gdk, GObject
+from terminatorlib.util import err, dbg
+from terminatorlib.version import APP_NAME
 
-if float(APP_VERSION) <= 0.98:
-    import gtk as Gtk
-else:
-    import gi
-    from gi.repository import Gtk, Gdk, GObject, Notify
-    
 
-AVAILABLE = ['OneBindShortCut']
+try:
+    gi.require_version('Notify', '0.7')
+    from gi.repository import Notify
+    # Every plugin you want Terminator to load *must* be listed in 'AVAILABLE'
+    # This is inside this try so we only make the plugin available if pynotify
+    #  is present on this computer.
+    AVAILABLE = ['OneBindShortCut']
+except (ImportError, ValueError):
+    err('OneBindShortcut plugin unavailable as we cannot import Notify')
+
+
 
 class OneBindShortCut(plugin.Plugin):
     capabilities = ['terminal_menu']
@@ -23,7 +28,7 @@ class OneBindShortCut(plugin.Plugin):
             window.connect('key-press-event', self.onKeyPress)
         Notify.init(APP_NAME.capitalize())
 
-    def send_notify(self,msg):
+    def notify(self,msg):
         note = Notify.Notification.new(_(msg), '' ,'terminator')
         note.set_timeout(15)
         note.show()
@@ -32,26 +37,22 @@ class OneBindShortCut(plugin.Plugin):
         for t in Terminator().terminals:
             if t.terminator.groupsend != t.terminator.groupsend_type['all']:
                 t.key_broadcast_all()
-                self.send_notify('Broadcast All Activated')
+                self.notify('Broadcast All Activated')
             else:
                 t.key_broadcast_off()
-                self.send_notify('Broadcast All Desactivated')
+                self.notify('Broadcast All Desactivated')
             break
 
     def broadcast_group(self, widget):
         for t in Terminator().terminals:
             if t.terminator.groupsend != t.terminator.groupsend_type['group']:
                 t.key_broadcast_group()
-                self.send_notify('Broadcast Group Activated')
+                self.notify('Broadcast Group Activated')
             else:
                 t.key_broadcast_off()
-                self.send_notify('Broadcast Group Desactivated')
+                self.notify('Broadcast Group Desactivated')
             break
 
     def onKeyPress(self, widget, event):
-        if float(APP_VERSION) <= 0.98:
-            if (event.state & Gtk.gdk.CONTROL_MASK == Gtk.gdk.CONTROL_MASK) and (event.keyval == 81 or event.keyval == 113): #CTRL+Q
-                self.broadcast_all(widget)
-        else:
-            if (event.state & Gdk.ModifierType.CONTROL_MASK == Gdk.ModifierType.CONTROL_MASK) and (event.keyval == 81 or event.keyval == 113): #CTRL+Q
+        if (event.state & Gdk.ModifierType.CONTROL_MASK == Gdk.ModifierType.CONTROL_MASK) and (event.keyval == 81 or event.keyval == 113): #CTRL+Q
                 self.broadcast_all(widget)
